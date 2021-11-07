40734e588f87423231f58a527d67e089c0c13e84
mavroudigeorgia <mavroudigeorgia@gmail.com>
2021-11-08 01:37:37 +0200

    Add Demo v.0.2

--- src/main/java/gr/aueb/elearn/ch1/AddDemo.java
+++ src/main/java/gr/aueb/elearn/ch1/AddDemo.java
@@ -1,4 +1,13 @@
 package gr.aueb.elearn.ch1;
 
 public class AddDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int result;
+
+        result = a + b;
+
+        System.out.println(result);
+    }
 }
